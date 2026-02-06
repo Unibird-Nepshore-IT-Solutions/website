f5a24c7cef6c35c2784e282e860caee6c5e45e8e
roobeen-b <rubinbaidhya@gmail.com>
2026-02-06 17:23:31 +0545

    Added email us and saving csv file to local functionality

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function validateEmail(email: string) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+export function validatePhone(phone: string) {
+  const phoneRegex = /^\d{10}$/;
+  return phoneRegex.test(phone);
 }
